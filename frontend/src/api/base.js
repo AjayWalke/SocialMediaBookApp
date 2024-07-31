755ee59caefba3faa42407ccd0d07fc9ab8817d0
AjayWalke <ajaywalke49@gmail.com>
2024-07-31 22:52:17 +0530

    fix: production backend url changed to .env

--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://127.0.0.1:3000/"
-// export const baseURL = 'https://socialmediabookapp.onrender.com/'
+// export const baseURL = "http://127.0.0.1:3000/"
+export const baseURL = process.env.BACKEND_URL
 export const apiEndpointV0 = 'api/v0/'
